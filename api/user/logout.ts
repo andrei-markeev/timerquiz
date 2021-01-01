94d9cea49a56deeccb8169eb97ea316ba976b9b1
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2021-01-01 05:51:49 +0200

    bugfix import

--- api/user/logout.ts
+++ api/user/logout.ts
@@ -1,5 +1,5 @@
 import { createEndpoint } from "../../lib/Endpoint";
-import { logoutGet } from "../../server/logoutGet";
+import { logoutGet } from "../../server/LogoutGet";
 import { logoutPost } from "../../server/LogoutPost";
 
 export default createEndpoint({ get: logoutGet, post: logoutPost });
